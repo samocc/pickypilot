3ab52df212ff27a8465f670ebbcef8e2a45a0b86
Sam <sam.oneinstant@gmail.com>
2021-12-12 20:12:24 -0600

    Removed react-routing to test image loading in production.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.scss';
 import '@aws-amplify/ui-react/styles.css';
-import { Routes, Route } from "react-router-dom";
+// import { Routes, Route } from "react-router-dom";
 import Home from "./Home/Home";
-// import PickyRegister from "./register_wizard/PickyRegister";
-import RegisterPage from "./register_wizard/RegisterPage";
+// // import PickyRegister from "./register_wizard/PickyRegister";
+// import RegisterPage from "./register_wizard/RegisterPage";
 
 function App() {
     return (
@@ -13,10 +13,11 @@ function App() {
             {/*    <Link to="/">Home</Link>*/}
             {/*    <Link to="/register">Register</Link>*/}
             {/*</nav>*/}
-            <Routes>
-                <Route path="/" element={<Home/>} />
-                <Route path="/register" element={<RegisterPage/>} />
-            </Routes>
+            {/*<Routes>*/}
+            {/*    <Route path="/" element={<Home/>} />*/}
+            {/*    <Route path="/register" element={<RegisterPage/>} />*/}
+            {/*</Routes>*/}
+            <Home/>
         </div>
     );
 }
