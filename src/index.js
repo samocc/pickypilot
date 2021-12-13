fecdd6fe6ec7209e2dfd87b266a73cd9ef23db5a
Sam <sam.oneinstant@gmail.com>
2021-12-12 19:42:34 -0600

    Changed BrowserRouter for HashRouter to hopefully solve images not loading in production build.

--- src/index.js
+++ src/index.js
@@ -5,15 +5,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Amplify from 'aws-amplify';
 import config from './aws-exports';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 Amplify.configure(config);
 
 ReactDOM.render(
-    <BrowserRouter>
+    <HashRouter>
         <React.StrictMode>
             <App />
         </React.StrictMode>
-    </BrowserRouter>,
+    </HashRouter>,
   document.getElementById('root')
 );
 
